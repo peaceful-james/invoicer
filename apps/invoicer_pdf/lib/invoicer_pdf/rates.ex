455f1837afb3c26b909e8ed3ec271347dd503bbd
Peaceful James <james@peacefulprogramming.dev>
2021-11-11 02:38:12 +0000

    add semimonthly rate option

--- apps/invoicer_pdf/lib/invoicer_pdf/rates.ex
+++ apps/invoicer_pdf/lib/invoicer_pdf/rates.ex
@@ -2,20 +2,45 @@ defmodule InvoicerPdf.Rates do
   @moduledoc """
   Functions associated rates.
   """
-  alias InvoicerPdf.WorkingDays
-  @rate_types [:day, :month]
+  @rate_types [:day, :month, :semimonth]
+  @type rate :: unquote(Enum.reduce(@rate_types, &{:|, [], [&1, &2]}))
 
   @doc """
   A list of all the rate types
   """
   def rate_types, do: @rate_types
 
-  @spec total(:day | :month, Decimal.t(), %{
+  @doc """
+  Calculates the total amount to be charged depending
+  on the rate_type, rate_amount and days worked.
+  The logic for the difference rate_types is as follows:
+
+  - `:day` - the rate_amount is multiplied by the "days count",
+  which is calculated using the WorkingDays module
+
+  - `:month` - effectively, the given start_date and end_date
+  are diffed to produce a float which is multiplied by the monthly
+  rate. The result is usually very precise, and varies from invoice
+  to invoice if invoicing twice a month.
+  For example, given a start_date of 25th September and an
+  end_date of 10th October
+  let days_in_september = x
+  let days_in_october = y
+  then the total would be:
+  (((x - 25) / x) + (10 / y)) * monthly_rate
+
+  - `:semimonth` - the "approximate fornight diff" between
+  start_date and end_date is calculated as an integer, which is
+  then multiplied by the semimonthly rate.
+  This is useful for clients who want semimonthly invoices of
+  consistent amounts. For example, 10, 11, 12...20 days will all
+  result in the same total!
+  """
+  @spec total(:day | :month | :semimonth, Decimal.t(), %{
           days_count: integer(),
           start_date: Date.t(),
           end_date: Date.t()
         }) :: Decimal.t()
-
   def total(:day, rate_amount, %{days_count: days_count}) do
     Decimal.mult(rate_amount, days_count)
   end
@@ -23,6 +48,16 @@ defmodule InvoicerPdf.Rates do
   def total(:month, rate_amount, %{start_date: start_date, end_date: end_date}) do
     months(start_date, end_date)
     |> Decimal.mult(rate_amount)
+  end
+
+  def total(:semimonth, rate_amount, %{start_date: start_date, end_date: end_date}) do
+    approximate_number_of_fortnights =
+      end_date
+      |> Date.diff(start_date)
+      |> Kernel./(14.0)
+      |> round()
+
+    Decimal.mult(rate_amount, approximate_number_of_fortnights)
   end
 
   @spec months(Date.t(), Date.t(), Decimal.t()) :: Decimal.t()
